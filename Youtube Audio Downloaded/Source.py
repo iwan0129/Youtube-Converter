f71b3e48d90542fed72c507eb929cac1b846e0e7
Iwan0129 <uzunow0129@gmail.com>
2020-12-01 22:11:25 +0200

    Added Function To Check If User Input Is Valid Youtube URL

--- Youtube Audio Downloaded/Source.py
+++ Youtube Audio Downloaded/Source.py
@@ -1,21 +1,24 @@
+import re
+
 try:
     from pytube import YouTube
     from pytube import Playlist
 except Exception as ex:
     print("Missing modules: {0}".format(ex.args));
         
+def is_youtubeURL(str):
+   return re.findall('http[s]?://www.youtube.com/(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\), ]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', str)
+
 while True:
 
     url = input("Enter video url:");
 
-    try:
+    if (is_youtubeURL(url)):
+        try:
 
-        youtube = YouTube(url);
+            print(YouTube(url).streams.filter(only_audio=True, mime_type="audio/mp4").first().download());
 
-        print(youtube.streams.filter(only_audio=True, mime_type="audio/mp4").first().download());   
-
-    except Exception as ex:
-        if ex.pattern == "(?:v=|\\/)([0-9A-Za-z_-]{11}).*":
-            print("Error parsing url please enter valid url");    
-        else:
-            print("{0}".format(ex.args));     
+        except Exception as ex:
+            print("{0}".format(ex.args));       
+    else:
+        print("Please enter valid youtube url");
