30ce6c7e176d1bec6aa7710845733bb761fd08c1
Iwan0129 <uzunow0129@gmail.com>
2020-12-01 16:22:23 +0200

    Changed Code Format

--- Youtube Audio Downloaded/Source.py
+++ Youtube Audio Downloaded/Source.py
@@ -9,9 +9,11 @@ while True:
     url = input("Enter video url:");
 
     try:
+
         youtube = YouTube(url);
-        print(youtube.streams.filter(only_audio=True, mime_type="audio/mp4").first().download());
-    
+
+        print(youtube.streams.filter(only_audio=True, mime_type="audio/mp4").first().download());   
+
     except Exception as ex:
         if ex.pattern == "(?:v=|\\/)([0-9A-Za-z_-]{11}).*":
             print("Error parsing url please enter valid url");    
